1cc07149abb0c909ae3af08a2c7699774208ef3a
Fernando Torterolo <nandotorterolo@gmail.com>
2024-11-14 21:44:27 -0300

    try again with parameter true

--- integration/setupBifrost.sh
+++ integration/setupBifrost.sh
@@ -14,7 +14,6 @@ node:
     staker-count: 2
     local-staker-index: 0
     timestamp: $TIMESTAMP
-    regtest-enabled: true
     stakes: [10000, 10000]
 "
 echo > node02/config.yaml "\
@@ -23,11 +22,10 @@ node:
     staker-count: 2
     local-staker-index: 1
     timestamp: $TIMESTAMP
-    regtest-enabled: true
     stakes: [10000, 10000]
 "
 
-set CONTAINER_ID (docker run --rm -d --name node01 -p 9085:9085 -p 9084:9084 -p 9091:9091 -v (pwd)/node01:/staking:rw ghcr.io/plasmalaboratories/plasma-node:0.1.3 --  --config  /staking/config.yaml --regtest)
+set CONTAINER_ID (docker run --rm -d --name node01 -p 9085:9085 -p 9084:9084 -p 9091:9091 -v (pwd)/node01:/staking:rw ghcr.io/plasmalaboratories/plasma-node:0.1.3 --  --config  /staking/config.yaml --block-regtest-permission true)
 set IP_CONTAINER (docker network inspect bridge | jq  ".[0].Containers.\"$CONTAINER_ID\".IPv4Address" | sed  's:"::g' | sed -n 's:\(.*\)/.*:\1:p')
 echo "IP_CONTAINER: $IP_CONTAINER"
-docker run --rm -d --name node02 -e NODE_P2P_KNOWN_PEERS=$IP_CONTAINER:9085 -p 9087:9085 -p 9086:9084 -p 9092:9091 -v (pwd)/node02:/staking:rw ghcr.io/plasmalaboratories/plasma-node:0.1.3 --  --config  /staking/config.yaml --regtest
+docker run --rm -d --name node02 -e NODE_P2P_KNOWN_PEERS=$IP_CONTAINER:9085 -p 9087:9085 -p 9086:9084 -p 9092:9091 -v (pwd)/node02:/staking:rw ghcr.io/plasmalaboratories/plasma-node:0.1.3 --  --config  /staking/config.yaml --block-regtest-permission true
