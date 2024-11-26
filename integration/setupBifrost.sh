3eb23d97580ec4b0891ca6bac4fa788b417cbc32
Fernando <ft@plasma.to>
2024-11-26 15:10:49 -0300

    TSDK-933_ethPrivateNetwork (#59)

--- integration/setupBifrost.sh
+++ integration/setupBifrost.sh
@@ -25,7 +25,7 @@ node:
     stakes: [10000, 10000]
 "
 
-set CONTAINER_ID (docker run --rm -d --name node01 -p 9085:9085 -p 9084:9084 -p 9091:9091 -v (pwd)/node01:/staking:rw ghcr.io/plasmalaboratories/plasma-node:0.1.3 --  --config  /staking/config.yaml --block-regtest-permission true)
+set CONTAINER_ID (docker run --rm -d --name node01 -p 9085:9085 -p 9084:9084 -p 9091:9091 -v (pwd)/node01:/staking:rw ghcr.io/plasmalaboratories/plasma-node:0.1.4 --  --config  /staking/config.yaml --block-regtest-permission true)
 set IP_CONTAINER (docker network inspect bridge | jq  ".[0].Containers.\"$CONTAINER_ID\".IPv4Address" | sed  's:"::g' | sed -n 's:\(.*\)/.*:\1:p')
 echo "IP_CONTAINER: $IP_CONTAINER"
-docker run --rm -d --name node02 -e NODE_P2P_KNOWN_PEERS=$IP_CONTAINER:9085 -p 9087:9085 -p 9086:9084 -p 9092:9091 -v (pwd)/node02:/staking:rw ghcr.io/plasmalaboratories/plasma-node:0.1.3 --  --config  /staking/config.yaml --block-regtest-permission true
+docker run --rm -d --name node02 -e NODE_P2P_KNOWN_PEERS=$IP_CONTAINER:9085 -p 9087:9085 -p 9086:9084 -p 9092:9091 -v (pwd)/node02:/staking:rw ghcr.io/plasmalaboratories/plasma-node:0.1.4 --  --config  /staking/config.yaml --block-regtest-permission true
